4b2addb6f5bd0b6ae1a8e0fd21d42662adc20425
skythelimit24 <akash.tanwar101@gmail.com>
2022-08-27 10:32:35 +0530

    class based to function based

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,17 @@ import React, { Component } from "react"
 
 export class NewsItem extends Component {
     render() {
-        let {title, description,imageUrl,url} = this.props;
+        let { title,description,imageUrl,url, author,date,source} = this.props;
         return (
-            <div className="my-3">
+            <div className="my-3" >
                 <div className="card">
-                    <img src={!imageUrl?"https://cdn.cssauthor.com/wp-content/uploads/2018/06/Simple-Loader.gif?strip=all&lossy=1&resize=800%2C600&ssl=1":imageUrl} className="card-img-top" alt="..."/>
+                <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger"style={{left:'80%',zIndex:'1'}}>{source}
+                        </span>
+                    <img src={!imageUrl ? "https://cdn.cssauthor.com/wp-content/uploads/2018/06/Simple-Loader.gif?strip=all&lossy=1&resize=800%2C600&ssl=1":imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">Published By : <b>{!author ? "unknown" : author}</b> on =<b>{new Date(date).toGMTString()}</b></small></p>
                         <a href={url} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
